7ca2f7f813680df6cbc0c2111a38a8a0daaca999
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-26 20:09:27 -0600

    feat: Implemented change page within Topic layout
    Also it was styled and implemented return icon

--- frontend/src/pages/Topic/Topic.jsx
+++ frontend/src/pages/Topic/Topic.jsx
@@ -1,11 +1,15 @@
+import './Topic.scss'
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getPageContent } from "../../api/getPageContent"
 import List from "../../layout/List/List"
 import Loading from "../../components/Loading/Loading"
+import { FixedButton, ShortButton } from "../../components/Button/Button"
+import { ReactComponent as Return } from '../../assets/svg/return.svg'
 
 function Topic() {
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
   const [list, setList] = useState([])
 
   const params = useParams()
@@ -13,16 +17,39 @@ function Topic() {
   const topicPath = `/${topicContent}`
   
   useEffect(() => {
-    getPageContent(topicPath)
+    setLoading(true)
+
+    getPageContent(topicPath, page)
     .then(data => {
       setLoading(false)
 
       const dataList = data.data
       setList(dataList)
     })
-  }, [topicPath])
+  }, [page, topicPath])
 
-  if (loading) return <Loading/>
+  function scrollTopPage() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  function getMoreData() {
+    scrollTopPage()
+
+    setPage(previous => previous + 1)
+  }
+
+  function getPreviousData() {
+    scrollTopPage()
+    
+    if (page == 1) return null
+    
+    setPage(previous => previous - 1)
+  }
+
+  if (loading) return <Loading className="large-bold"/>
 
   return (
       <article id={topicContent} className="topic">
@@ -30,6 +57,14 @@ function Topic() {
           list={list}
           parentPath={topicPath}
         />
+        <section className="load-buttons">
+          <ShortButton onClick={getPreviousData}>
+            <Return/>
+          </ShortButton>
+          <FixedButton onClick={getMoreData}>
+            Load More!
+          </FixedButton>
+        </section>
       </article>
   )
 }
