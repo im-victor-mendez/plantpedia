b5220d0b96bc2c71c85eecf07d7f93312896680f
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-29 14:58:49 -0600

    ci: Defined Default and Opened TopBar

--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -1,19 +1,69 @@
 import './TopBar.scss'
-import back_icon from '../../assets/svg/back.svg'
+
 import PropTypes from 'prop-types'
+
+import back_icon from '../../assets/svg/back.svg'
+import { ReactComponent as MenuIcon } from '../../assets/svg/menu.svg'
+import { ReactComponent as LeafIcon } from '../../assets/svg/leaf.svg'
+import { ReactComponent as CloseIcon } from '../../assets/svg/close.svg'
+import { ReactComponent as AccountIcon } from '../../assets/svg/account.svg'
 
 /**
  * TopBar structure
+ * @description To use to create all TopBar variations
+ * @param {any} Destructured_Props
+ * @param {string} type Type of TopBar component to use styles
  * @param {any} children Needs for all of other components
  * @returns {React.JSX.Element}
  */
-function TopBar({ children }) {
+function TopBar({ type, children }) {
   return (
-    <div className="top-bar">{children}</div>
+    <div className={`top-bar ${type ? type : ''}`}>
+        {children}
+    </div>
   )
 }
 TopBar.propTypes = {
+    type: PropTypes.string,
     children: PropTypes.any
+}
+
+/**
+ * Default TopBar
+ * @description To use in Home page
+ * @param {any} Destructured_Props
+ * @param {Function} openMenu Function to set state that manage display of menu true
+ * @returns {React.JSX.Element}
+ */
+export function Default({ openMenu }) {
+    return (
+        <TopBar type='default'>
+            <MenuIcon onClick={openMenu}/>
+            <LeafIcon/>
+        </TopBar>
+    )
+}
+Default.propTypes = {
+    openMenu: PropTypes.func
+}
+
+/**
+ * Opened TopBar
+ * @description To use when Default TopBar was activated
+ * @param {any} Destructured_Props
+ * @param {Function} closeMenu Function to set state that manage display of menu false
+ * @returns {React.JSX.Element}
+ */
+export function Opened({ closeMenu }) {
+    return (
+        <TopBar type='opened'>
+            <CloseIcon onClick={closeMenu}/>
+            <AccountIcon/>
+        </TopBar>
+    )
+}
+Opened.propTypes = {
+    closeMenu: PropTypes.func
 }
 
 /**
