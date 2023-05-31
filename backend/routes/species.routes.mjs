b98255a43c0f64ce1319e5f059ba1a771bd6c7fc
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-31 13:28:32 -0600

    fix: Fixed error handling

--- backend/routes/species.routes.mjs
+++ backend/routes/species.routes.mjs
@@ -4,6 +4,7 @@ import { getSpecies } from "./controllers/species.controller.mjs";
 const speciesRouter = Router()
 const path = '/species'
 
+// Get all species
 speciesRouter.get(path, getSpecies)
 
 export default speciesRouter
